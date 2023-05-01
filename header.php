eaeff64f003c29bba1d205b996b5ddeb16b5d287
mehul <rathodmehul782@gmail.com>
2023-05-01 15:16:50 +0530

    header
    set line-height 67px

--- header.php
+++ header.php
@@ -177,7 +177,7 @@ session_start();
 						<div class="col-md-3">
 							<div class="header-logo">
 								<a href="#" class="logo">
-								<font style="font-style:normal; font-size: 33px;color: aliceblue;font-family: serif">
+								<font style="line-height:67px; font-style:normal; font-size: 33px;color: aliceblue;font-family: serif">
                                         Online Shop
                                     </font>
 									
